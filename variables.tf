c128e17465ad3219b24ae4f24163bce78f86539e
dorra-ferah <123673334+dorra-ferah@users.noreply.github.com>
2024-10-01 10:12:05 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,9 @@
-variable "project_id" {
+variable "DEVSHELL_PROJECT_ID" {
   description = "Google Cloud project ID"
   type        = string
 
+}
+variable "gitlab_pipeline_id" {
+  description = "L'ID du pipeline GitLab utilisé pour l'authentification avec WIF"
+  type        = string
 }
